27298b78e4f1f4b1bd660f8a1ebe3b5d747d3a08
bentan4853563 <bentan010918+1@gmail.com>
2024-06-13 19:41:52 +1000

    Simple react tailwind project to calculate simple operations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleAdd = () => {
+    setValue(value + 1);
+  };
+
+  const handleSub = () => {
+    setValue(value - 1);
+  };
+
+  const handleMul = () => {
+    setValue(value * 2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-[100vh] App flex flex-col items-center justify-center gap-8">
+      <div className="flex items-center gap-8">
+        <button
+          id="add"
+          onClick={handleAdd}
+          className="w-12 h-12 bg-red-600 text-white p-2 rounded-full border border-black cursor-pointer"
         >
-          Learn React
-        </a>
-      </header>
+          +1
+        </button>
+        <button
+          id="sub"
+          onClick={handleSub}
+          className="w-12 h-12 bg-red-600 text-white p-2 rounded-full border border-black cursor-pointer"
+        >
+          -1
+        </button>
+        <button
+          id="mul"
+          onClick={handleMul}
+          className="w-12 h-12 bg-red-600 text-white p-2 rounded-full border border-black cursor-pointer"
+        >
+          *2
+        </button>
+      </div>
+      <div
+        id="display"
+        className="w-24 h-12 flex items-center justify-center text-xl border border-black"
+      >
+        {value}
+      </div>
     </div>
   );
 }
